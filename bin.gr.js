122621f73c4cd1f9c61ce229a05404ba35c6eef9
The Right Honourable Reverend <samueljsmith@gmail.com>
2022-02-21 04:00:00 -0600

    Update bin.gr.js

--- bin.gr.js
+++ bin.gr.js
@@ -1,8 +1,19 @@
 /** @param {NS} ns **/
+
+const runtimeMultiplier = 3.2;
+
 export async function main(ns) {
 	let target = ns.args[0];
 	let repeat = ns.args[1];
+	let batch_land = ns.args[2];
+
+	let runtime = runtimeMultiplier * ns.getHackTime(target);
+
 	do {
+		if (batch_land) {
+			let currentTime = performance.now()
+			await ns.sleep(batch_land - currentTime - runtime)
+		}
 		await ns.grow(target)
 	} while (repeat)
 }
